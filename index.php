502a53b66182287dcd1471af38e5177cadd1a144
expertlight <iam32five@gmail.com>
2012-01-17 18:41:44 +0300

    1) добавлена бд, добавлен ярлык 2) наваял немного бд

--- index.php
+++ index.php
@@ -1,5 +1,17 @@
+<html>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />    
 <?php
-
-echo 1;
-
+    $host="localhost";
+    $user="root";
+    $pass="";
+    $db=mysql_connect($host,$user,$pass);
+    mysql_select_db("in40",$db);
+    mysql_query("SET NAMES utf8");
+    $categories_t=mysql_query("SELECT * FROM categories");	
+    
+    while($row = mysql_fetch_array($categories_t))
+    {
+        echo $row['name']."<br>";
+    }
 ?>
+</html>
